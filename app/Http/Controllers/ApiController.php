57ef2b6f5183691ff75b29dad91e3aeb12cd2670
Paulo Eremita <eremitaio79@gmail.com>
2025-02-06 22:52:23 -0300

    Primeiras configurações de API
    Primeiras configurações de API.

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Método status.
+     */
+    public function status()
+    {
+        // return response()->json(['status' => 'OK']);
+        return "Status OK dentro do método status.";
+    }
+
+    /**
+     * Retorna todos os clientes cadastrados.
+     */
+    public function clients()
+    {
+        // return response()->json(['clients' => []]);
+        return "Todos os clientes cadastrados. Retorno do método clients.";
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
